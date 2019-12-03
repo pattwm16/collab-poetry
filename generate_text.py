61e4a9e90e9e9848c50da4b88b5bf84161fa26c7
Will Patterson <pattwm16@gmail.com>
2019-12-03 13:20:03 -0500

    final touch to generate_text.py
    create cli interface for generate_text.py to show in class

--- generate_text.py
+++ generate_text.py
@@ -4,49 +4,61 @@ import requests
 import re
 from blessings import Terminal
 import logging
+import textwrap 
 
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
-# logging.getLogger('tensorflow').setLevel(logging.FATAL)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
+logging.getLogger('tensorflow').setLevel(logging.FATAL)
 
 # Helper functions ---
 def corr(s):
-    return re.sub(r'\.(?! )', '. ', re.sub(r' +', ' ', s))
+	return re.sub(r'\.(?! )', '. ', re.sub(r' +', ' ', s))
 
-def generate_text(user_inp, out_len=100):
-    """consumes user text, returns short generated text
-    param user_inp: user provided input seed
-    param out_len: user provided length for output (default 100)
-    returns: output string of length out_len
-    """
-    text = gpt2.generate(sess,
-                  length=out_len,
-                  temperature=0.7,
-                  prefix=user_inp,
-                  nsamples=5,
-                  batch_size=5,
-                  return_as_list=True)[0]
-    return corr(text)
+def generate_text(user_inp, out_len=300):
+	"""consumes user text, returns short generated text
+	param user_inp: user provided input seed
+	param out_len: user provided length for output (default 100)
+	returns: output string of length out_len
+	"""
+	text = gpt2.generate(sess,
+				  length=out_len,
+				  temperature=0.74,
+				  prefix=user_inp,
+				  nsamples=5,
+				  batch_size=5,
+				  return_as_list=True)[0]
+	return corr(text)
 
 # Code body ---
 # instantiate the model session
 sess = gpt2.start_tf_sess()
 # load the model
 gpt2.load_gpt2(sess,
-              run_name="run1",
-              checkpoint_dir="checkpoint")
+			  run_name="run2",
+			  checkpoint_dir="checkpoint")
 
 # start cli loop
+wrapper = textwrap.TextWrapper(width=50) 
 term = Terminal()
+os.system('cls' if os.name == 'nt' else 'clear')
 while True:
-    with term.location(0, term.height - 1):
-        print('This is', term.underline('pretty!'))
-        print(term.red + term.on_green + 'Red on green? Ick!' + term.normal)
-        print(term.bright_red + term.on_bright_blue + 'This is even worse!' + term.normal)
-        print("Enter your prompt:")
-        prompt = str(input())
-        if prompt == "end_program":
-            break
-        print(generate_text(prompt), flush=True)
+	with term.location(0, term.height - 1):
+		print(term.cyan + term.bold + "Tell me your full name:" + term.normal)
+		name = str(input())
+		print(term.cyan + term.bold + "How should we start our poem?" + term.normal)
+		prompt = str(input())
+		if prompt == "end_program":
+			break
+		output = generate_text(prompt)
+		string = wrapper.fill(text=output)
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print("{}, by {}".format(prompt, name))
+		print("--------------------------------------------------")
+		print(string, flush=True)
+		print("--------------------------------------------------")
+
+		input(term.bold("\nPress Enter to continue..."))
+		os.system('cls' if os.name == 'nt' else 'clear')
 
 
 print(corr(text))
